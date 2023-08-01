6abdf950ddb758ba1108e34ba9d5f5bbe70ed3ce
Ahsan2001 <ahsan.sabir@yahoo.com>
2023-08-01 13:29:06 -0700

    completed project 05

--- currency_convert_05/index.ts
+++ currency_convert_05/index.ts
@@ -1,39 +1,87 @@
-import inquirer from "inquirer";
-import chalk from "chalk";
+#! /usr/bin/env node
 
+
+import inquirer from "inquirer";
+import chalk from 'chalk';
 
 async function handleInput() {
 
     const currency = await inquirer.prompt([
         {
-            type: "list",
-            name: "currency_1",
-            message: chalk.green("Enter your current currency :"),
-            choices: ['PKR','USD','EUR','GBP','PLN','CAD','JPY','EGY']
-        },
-        {
-            type: "list",
-            name: "currency_2",
-            message: chalk.green("Enter currency you want to convert :"),
-            choices: ['PKR','USD','EUR','GBP','PLN','CAD','JPY','EGY']
-        },
-        {
             type: "string",
             name: "amount",
-            message: chalk.green("Enter the amount you want to convert :"),
-        }
+            message: chalk.green("Enter the amount in number you want to convert :"),
+        },
+        {
+            type: "list",
+            name: "currency_1",
+            message: chalk.green("Enter currency you want to check the monetary value of"),
+            choices: ['USD', 'EUR', 'CAD', 'JPY', 'EGY']
+        },
     ])
 
     if
-    (currency.amount === "") {
-        console.log(chalk.red("Please insert amount !"));
+        (currency.amount === "") {
+        console.log("-----------------------------------------------------------")
+        console.log("-----------------------------------------------------------")
+        chalk.red("Please insert amount !");
         return false;
     }
 
 
+    switch (currency.currency_1) {
+        case "USD": {
+            console.log("---------------- Successfully coverted --------------------")
+            console.log("-----------------------------------------------------------")
+            console.log(chalk.green.bold(`Your ${currency.amount} PKR is equal to: ${(currency.amount / 287.05).toFixed(2)} usd`))
+            break;
+        }
+        case "EUR": {
+            console.log("---------------- Successfully coverted --------------------")
+            console.log("-----------------------------------------------------------")
+            console.log(chalk.green.bold(`Your ${currency.amount} PKR is equal to: ${(currency.amount / 315.31).toFixed(2)} euro`))
+            break;
+        }
+        case "CAD": {
+            console.log("---------------- Successfully coverted --------------------")
+            console.log("-----------------------------------------------------------")
+            console.log(chalk.green.bold(`Your ${currency.amount} PKR is equal to: ${(currency.amount / 216.03).toFixed(2)} cad`))
+            break;
+        }
+        case "JPY": {
+            console.log("---------------- Successfully coverted --------------------")
+            console.log("-----------------------------------------------------------")
+            console.log(chalk.green.bold(`Your ${currency.amount} PKR is equal to: ${(currency.amount / 2.00).toFixed(2)} jpy`))
+            break;
+        }
+        case "EGY": {
+            console.log("---------------- Successfully coverted --------------------")
+            console.log("-----------------------------------------------------------")
+            console.log(chalk.green.bold(`Your ${currency.amount} PKR is equal to: ${(currency.amount / 9.29).toFixed(2)} egy`))
+            break;
+        }
+        default: {
+            console.log("-------------------- Success fAILED -----------------------")
+            console.log("-----------------------------------------------------------")
+            console.log(chalk.red("Please insert amount !"));
+            break;
+        }
+    }
 
 }
 
 
+async function Run() {
+    let again;
+    do {
+        await handleInput();
+        again = await inquirer.prompt([{
+            type: "string",
+            name: "q1",
+            message: chalk.bold("Want to use again this app ? press yes or no")
+        }])
 
-handleInput()
+    } while (again.q1.toLowerCase() === "yes" || again.q1.toLowerCase() === "y" );
+}
+
+Run()
--- currency_convert_05/readme.md
+++ currency_convert_05/readme.md
@@ -1,16 +1,2 @@
 # Running command
      npx ahsan-currency-converter
-
-
-
-<!--  top 10 currencies
-1. Kuwaiti Dinar (KWD)
-2. Bahraini Dinar (BHD)
-3. Omani Rial (OMR)
-4. Jordanian Dinar (JOD)
-5. British Pound (GBP)
-6. (tie) Cayman Islands Dollar (KYD)
-7. (tie) Gibraltar Pound (GIP)
-8. (tie) Swiss Franc (CHF)
-9. (tie) Euro (EUR)
-10. U.S. Dollar (USD) -->
