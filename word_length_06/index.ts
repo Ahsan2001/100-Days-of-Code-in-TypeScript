218ca8659fc95300703caf7c232f705aa2f35065
Ahsan2001 <ahsan.sabir@yahoo.com>
2023-08-01 18:00:39 -0700

    nhi howa sad loif

---
+++ word_length_06/index.ts
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+import chalk from 'chalk';
+
+async function handleInput() {
+
+    const word = await inquirer.prompt([
+        {
+            type: "string",
+            name: "paragraph",
+            message: chalk.green("Enter the paragraph :"),
+        },
+    ])
+
+    if
+        (word.paragraph === "") {
+        chalk.red("Please insert some text here !");
+        return false;
+    }
+
+    let removeSpace = word.paragraph.trim(); 
+
+    console.log(removeSpace)
+
+
+
+
+}
+
+
+async function Run() {
+    let again;
+    do {
+        await handleInput();
+        again = await inquirer.prompt([{
+            type: "string",
+            name: "q1",
+            message: chalk.bold("Want to use again this app ? press yes or no")
+        }])
+
+    } while (again.q1.toLowerCase() === "yes" || again.q1.toLowerCase() === "y" );
+}
+
+Run()
